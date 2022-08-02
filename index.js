bc1d72ece2ef335648e7bfb3d6095214c1c358be
lauradefreitas2 <lauradovalegameplays@gmail.com>
2022-08-02 10:24:35 -0300

    atividade completa

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let vetor = [1,2,3]
 let soma = 0
 
 for (let  i= 0; i < vetor.length; i++) {
-    const element = array[index];
-    vetor += soma
-    console.log(soma)
-}
+    vetor += soma[i]
+};
+
+console.log(soma)
